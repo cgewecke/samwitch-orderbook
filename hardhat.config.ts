5d95c3b0fbd13db79ee9cc87db82d1c2567ae504
cgewecke <christophergewecke@gmail.com>
2024-02-27 20:32:42 -0800

    Run in CI

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,11 @@
 import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
-import "hardhat-abi-exporter";
-import "hardhat-contract-sizer";
-import "hardhat-storage-layout";
+//import "hardhat-abi-exporter";
+//import "hardhat-contract-sizer";
+//import "hardhat-storage-layout";
 import "solidity-coverage";
-import "@primitivefi/hardhat-dodoc";
+//import "@primitivefi/hardhat-dodoc";
 import {SolcUserConfig} from "hardhat/types";
 import "dotenv/config";
 
@@ -20,7 +20,7 @@ const defaultConfig: SolcUserConfig = {
         yul: true,
       },
     },
-    viaIR: process.env.HARDHAT_VIAIR != "false", // This cannot be used with coverage for instance
+    viaIR: true,
     outputSelection: {
       "*": {
         "*": ["storageLayout"],
@@ -76,14 +76,14 @@ const config: HardhatUserConfig = {
     gasPriceApi: "https://api.ftmscan.com/api?module=proxy&action=eth_gasPrice",
     coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
-  contractSizer: {
+  /*contractSizer: {
     alphaSort: true,
     runOnCompile: true,
     disambiguatePaths: false,
-  },
-  dodoc: {
+  },*/
+  /*dodoc: {
     include: ["IBrushToken.sol", "ISamWitchOrderBook.sol", "SamWitchOrderBook.sol"],
-  },
+  },*/
   networks: {
     hardhat: {
       gasPrice: 0,
@@ -91,7 +91,7 @@ const config: HardhatUserConfig = {
       blockGasLimit: 30000000,
       allowUnlimitedContractSize: true,
     },
-    fantom: {
+    /*fantom: {
       url: process.env.FANTOM_RPC,
       accounts: [process.env.PRIVATE_KEY as string],
     },
@@ -102,16 +102,16 @@ const config: HardhatUserConfig = {
     fantom_sonic_testnet: {
       url: process.env.FANTOM_SONIC_TESTNET_RPC,
       accounts: [process.env.PRIVATE_KEY as string],
-    },
+    },*/
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
-  abiExporter: {
+  /*abiExporter: {
     path: "./data/abi",
     clear: true,
     flat: false,
-  },
+  },*/
 };
 
 export default config;
